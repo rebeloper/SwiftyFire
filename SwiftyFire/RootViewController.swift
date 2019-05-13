b086e81aa6c2c89df5d8c83ce9d11c503c7d72ad
Alex <therebeloper@gmail.com>
2019-05-13 08:55:01 +0300

    Initial Commit

---
+++ SwiftyFire/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  SwiftyFire
+//
+//  Created by Alex Nagy on 13/05/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
